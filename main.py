6e0b3e2b24f5e35de806b4b9681f89251704769f
Mike Fuller <mike@finops.org>
2023-05-31 14:33:39 +1000

    added time to xml

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ import xml.etree.cElementTree as ET
 
 testsuites = ET.Element("testsuites")
 
-testsuite1 = ET.SubElement(testsuites, "testsuite", name="Tests.Registration")
-testcase1 = ET.SubElement(testsuite1, "testcase", name="testCase1")
-testcase2 = ET.SubElement(testsuite1, "testcase", name="testCase2")
+testsuite1 = ET.SubElement(
+    testsuites, "testsuite", name="Tests.Registration", time="0.008"
+)
+testcase1 = ET.SubElement(testsuite1, "testcase", name="testCase1", time="0.004")
+testcase2 = ET.SubElement(testsuite1, "testcase", name="testCase2", time="0.004")
 ET.SubElement(
     testsuite1,
     "failure",
