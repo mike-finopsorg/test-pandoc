870e4ef3286443d6353209e6249578d787bba023
Mike Fuller <mike@finops.org>
2023-05-31 14:40:36 +1000

    time work

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 import xml.etree.cElementTree as ET
 
-testsuites = ET.Element("testsuites")
+testsuites = ET.Element(
+    "testsuites",
+    name="FOCUS Validations",
+    tests="2",
+    failures="1",
+    errors="0",
+    skipped="0",
+    assertions="2",
+    time="0",
+    timestamp="2021-04-02T15:48:23",
+)
 
 testsuite1 = ET.SubElement(
     testsuites, "testsuite", name="Tests.Registration", time="0.008"
