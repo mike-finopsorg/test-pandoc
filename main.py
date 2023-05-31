e875514764d2fb608d277dc8136cbe98e35596a0
Mike Fuller <mike@finops.org>
2023-05-31 14:07:58 +1000

    test xml

---
+++ main.py
@@ -0,0 +1,19 @@
+import xml.etree.cElementTree as ET
+
+testsuites = ET.Element("testsuites")
+
+testsuite1 = ET.SubElement(testsuites, "testsuite", name="Tests.Registration")
+testcase1 = ET.SubElement(testsuite1, "testcase", name="testCase1")
+testcase2 = ET.SubElement(testsuite1, "testcase", name="testCase2")
+ET.SubElement(
+    testsuite1,
+    "failure",
+    name="testCase2",
+    message="Expected value did not match.",
+    type="AssertionError",
+)
+
+
+tree = ET.ElementTree(testsuites)
+ET.indent(tree)
+tree.write("filename.xml", encoding="utf-8", xml_declaration=True)
