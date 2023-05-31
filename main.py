58af5e028c492375fdd421089fdca246737c8531
Mike Fuller <mike@finops.org>
2023-05-31 14:55:15 +1000

    get failures and skipped working

--- main.py
+++ main.py
@@ -3,29 +3,28 @@ import xml.etree.cElementTree as ET
 testsuites = ET.Element(
     "testsuites",
     name="FOCUS Validations",
-    tests="2",
+    tests="3",
     failures="1",
     errors="0",
-    skipped="0",
-    assertions="2",
+    skipped="1",
+    assertions="3",
     time="0",
     timestamp="2021-04-02T15:48:23",
 )
 
-testsuite1 = ET.SubElement(
-    testsuites, "testsuite", name="Tests.Registration", time="0.008"
-)
-testcase1 = ET.SubElement(testsuite1, "testcase", name="testCase1", time="0.004")
-testcase2 = ET.SubElement(testsuite1, "testcase", name="testCase2", time="0.004")
+testsuite1 = ET.SubElement(testsuites, "testsuite", name="ColumnA", time="0.008")
+testcase1 = ET.SubElement(testsuite1, "testcase", name="NotNull", time="0")
+testcase2 = ET.SubElement(testsuite1, "testcase", name="UniqueValues", time="0")
 ET.SubElement(
-    testsuite1,
+    testcase2,
     "failure",
     name="testCase2",
     message="Expected value did not match.",
     type="AssertionError",
 )
-
+testcase3 = ET.SubElement(testsuite1, "testcase", name="Range", time="0.004")
+ET.SubElement(testcase3, "skipped", message="Test was skipped.")
 
 tree = ET.ElementTree(testsuites)
 ET.indent(tree)
-tree.write("reports/focus_validations.xml", encoding="utf-8", xml_declaration=True)
+tree.write("focus_validations.xml", encoding="utf-8", xml_declaration=True)
